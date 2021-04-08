153ff255ca8b7703f429f5610b3cb93a11ebf6c2
taras.nechytailo <taras.nechytailo@agilie.com>
2021-04-08 16:26:46 +0300

    removed comment

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
   output: {
     file: 'dist/plugin.js',
     format: 'iife',
-    name: 'galleryVideoPicker', // TODO: change this
+    name: 'galleryVideoPicker',
     globals: {
       '@capacitor/core': 'capacitorExports',
     },
